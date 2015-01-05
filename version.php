677075dd71aa376e3302d65861482c1e0b9570db
Andraž <atletek@gmail.com>
2015-01-05 10:10:38 +0100

    First commit

---
+++ version.php
@@ -0,0 +1,18 @@
+report/reportbadges
+
+<?php
+
+////////////////////////////////////////////////////////////////////////////////
+//  Code fragment to define the module version etc.
+//  This fragment is called by /admin/index.php
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @package report_reportbadges
+ * @copyright 2015 Andraž Prinčič <atletek@gmail.com>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+$module->version = 2015010500;
+$module->requires = 2013111800;  // Requires this Moodle 2.X version
+$module->release = '2.7';
+$module->maturity = MATURITY_STABLE;
