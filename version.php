ed3d4c5fd90c1c2db45df25113ebfbd3dc804f46
Andraž <atletek@gmail.com>
2016-01-18 07:32:03 +0000

    Upgraded to support Moodle 3.0.

--- version.php
+++ version.php
@@ -10,9 +10,10 @@
  * @copyright 2015 Andraž Prinčič <atletek@gmail.com>
  * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-$module->version = 2015010504;
-$module->requires = 2013111800;  // Requires this Moodle 2.X version
-$module->release = '2.7';
-$module->maturity = MATURITY_ALPHA;
+$plugin->version = 2015010504;
+$plugin->requires = 2013111800;  // Requires this Moodle 2.X version
+$plugin->release = '2.7';
+$plugin->maturity = MATURITY_ALPHA;
+$plugin->component = 'report_reportbadges';
 
 //MATURITY_ALPHA, MATURITY_BETA, MATURITY_RC or MATURITY_STABLE
